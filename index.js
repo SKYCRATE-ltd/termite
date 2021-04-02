dab1f43cf14daac7228dd1b84c447efc25881a06
dumpstergoon <graham_daniel_robertson@protonmail.com>
2021-04-02 21:27:09 +0100

    basic Program

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-import Router from "./src/Router.js";
+import {
+	Procedure
+} from "zed";
+
+// A singleton. Creates a custom Type then returns that instance
+// Instance returned is a function.
+export default Procedure(
+	'Program',
+	{
+		init(cmds) {
+			return (...args) => {
+				if (args.length === 0)
+					args = process.argv.slice(2);
+				
+				if (typeof cmds === "function")
+					cmds(...args);
+				else {
+					const cmd = args.shift();
+					if (cmd && cmds[cmd])
+						cmds[cmd](...args);
+					else
+						this.error(`command "${cmd}" not found.`);
+				}
+			}
+		},
+		render(msg) {
+			console.log(msg);
+		},
+		error(msg) {
+			this.render(`ERROR: ${msg}`);
+		}
+	},
+);
